661e70f3db3a324479f1b2a0655a495f30948033
Lee Goddard <lee@leegoddard.net>
2014-07-10 21:26:14 +0100

    No jQuery

--- js/Ls.js
+++ js/Ls.js
@@ -1,38 +1,56 @@
-define(['jquery'], function (jQuery) {
-	/*
-		@param uri [string] - uri of `mod_autoindex` directory or similar HTML page
-		@param re [RegExp] - hyperlinks for files to select from directory listing
-		@param next [function] - callback
-	*/
-	var Ls = function (properties) {
+// AMD stub?
+var define = define || function (deps,module){ return module }
+
+define([], function () {
+    /*
+        @param uri [string] - uri of `mod_autoindex` directory or similar HTML page
+        @param re [RegExp] - hyperlinks for files to select from directory listing
+        @param next [function] - optional callback
+        @param error [function] - optional callback
+    */
+    var Ls = function (properties) {
+        var self = this;
         if (typeof properties !== 'object'){
             throw new TypeError('Ls requires an object as sole parameter.')
         }
-		var self = this;
+        this.re    = properties.re || /^.+\.(png|jpg|gif)$/;
+        this.next  = properties.next || function () {};
+        this.uris  = [];
+        this.error = properties.error || function (e) {
+            throw new Error('Error getting '+self.uri);
+        };
+        this.uri   = properties.uri;
+        if (! this.hasOwnProperty('uri')){
+            throw new TypeError('Ls requires a uri in its properties argument.')
+        }
 
-		this.uri = properties.uri;
-		this.re = properties.re || /^.+\.(png|jpg|gif)$/;
-		this.next = properties.next || function () {};
-		this.uris = [];
+        var req = new XMLHttpRequest();
+        req.open('GET', this.uri);
 
-		if (! this.hasOwnProperty('uri')){
-			throw new TypeError('Ls requires a uri in its properties argument.')
-		}
+        req.onload = function () {
+            if (req.status == 200) {
+                var html = document.createElement('div');
+                html.innerHTML = req.response;
+                var anchorNodeList = html.querySelectorAll( 'a[href]' );
+                for (var i = 0; i < anchorNodeList.length; ++i) {
+                    var href = anchorNodeList[i].getAttribute('href');
+                    if (href.match( self.re )){
+                        self.uris.push( self.uri +'/'+ href );
+                    }
+                }
+                self.next( self.uris );
+            }
+            else {
+                this.error(req.statusText);
+            }
+        };
 
-		jQuery.get( this.uri, function (html) {
-			jQuery('<html/>', { html: html })
-				.find('a')
-				.each( function(i,el){
-					// Only links to images
-					if (jQuery(el).attr('href').match( self.re )){
-						self.uris.push( self.uri +'/'+ jQuery(el).attr('href') );
-					}
-				}
-			);
+        req.onerror = function() {
+            this.error(req.statusText);
+        };
 
-			self.next( self.uris );
-		});
-	}
+        req.send();
+    };
 
-	return Ls;
+    return Ls;
 });
