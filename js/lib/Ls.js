b00dd7b2ed5f04705aadaa6f702d79889ab657a5
Lee Goddard <lee@leegoddard.net>
2014-10-11 11:30:04 +0100

    Polymorphic constructor

--- js/lib/Ls.js
+++ js/lib/Ls.js
@@ -1,61 +1,69 @@
 // AMD stub?
-var define = define || function (deps,module){ return module }
+var define = define || function ( deps, module ) {
+	return module
+}
 
-define([], function () {
-    /*
+define( [], function () {
+	/*
         @param uri [string] - uri of `mod_autoindex` directory or similar HTML page
         @param re [RegExp] - hyperlinks for files to select from directory listing
         @param next [function] - optional callback
         @param error [function] - optional callback
     */
-    var Ls = function (properties) {
-        console.log('Ls new - enter');
-        var self = this;
-        if (typeof properties !== 'object'){
-            throw new TypeError('Ls requires an object as sole parameter.')
-        }
-        this.re    = properties.re || /^.+\.(png|jpg|gif)$/;
-        this.next  = properties.next || function () {};
-        this.uris  = [];
-        this.error = properties.error || function (e) {
-            throw new Error('Error getting '+self.uri);
-        };
-        this.uri   = properties.uri;
-        if (! this.hasOwnProperty('uri')){
-            throw new TypeError('Ls requires a uri in its properties argument.')
-        }
+	var Ls = function ( properties ) {
+		console.log( 'Ls new - enter' );
+		var self = this;
 
-        var req = new XMLHttpRequest();
-        req.open('GET', this.uri);
+		if ( typeof properties !== 'string' && typeof properties !== 'object' ) {
+			throw new TypeError( 'Ls requires an object or string as sole parameter.' )
+		}
+        // if ( typeof properties === 'string' ) {
+        //  properties = {
+        //      uri: properties
+        //  };
+        // }
 
-        req.onload = function () {
-            console.log('Ls.onload enter');
-            if (req.status == 200) {
-                var html = document.createElement('div');
-                html.innerHTML = req.response;
-                var anchorNodeList = html.querySelectorAll( 'a[href]' );
-                for (var i = 0; i < anchorNodeList.length; ++i) {
-                    var href = anchorNodeList[i].getAttribute('href');
-                    if (href.match( self.re )){
-                        self.uris.push( self.uri +'/'+ href );
-                    }
-                }
-                console.log('Ls.onload leave, calling this.next with this.uris');
-                return self.next( self.uris );
-            }
-            else {
-                this.error(req.statusText);
-                return self;
-            }
-        };
+		this.re = properties.re || /^.+\.(png|jpg|gif)$/;
+		this.next = properties.next || function () {};
+		this.uris = [];
+		this.error = properties.error || function ( e ) {
+			throw new Error( 'Error getting ' + self.uri );
+		};
+		this.uri = properties.uri;
+		if ( !this.hasOwnProperty( 'uri' ) ) {
+			throw new TypeError( 'Ls requires a uri in its properties argument.' )
+		}
 
-        req.onerror = function() {
-            this.error(req.statusText);
-        };
+		var req = new XMLHttpRequest();
+		req.open( 'GET', this.uri );
 
-        req.send();
-        console.log('Ls new - leave with',this);
-    };
+		req.onload = function () {
+			console.log( 'Ls.onload enter' );
+			if ( req.status == 200 ) {
+				var html = document.createElement( 'div' );
+				html.innerHTML = req.response;
+				var anchorNodeList = html.querySelectorAll( 'a[href]' );
+				for ( var i = 0; i < anchorNodeList.length; ++i ) {
+					var href = anchorNodeList[ i ].getAttribute( 'href' );
+					if ( href.match( self.re ) ) {
+						self.uris.push( self.uri + '/' + href );
+					}
+				}
+				console.log( 'Ls.onload leave, calling this.next with this.uris' );
+				return self.next( self.uris );
+			} else {
+				this.error( req.statusText );
+				return self;
+			}
+		};
 
-    return Ls;
-});
+		req.onerror = function () {
+			this.error( req.statusText );
+		};
+
+		req.send();
+		console.log( 'Ls new - leave with', this );
+	};
+
+	return Ls;
+} );
